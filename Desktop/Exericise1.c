9f69fb4f99e12d9df17a5d8eb091814b13b3acea
root <root@localhost.localdomain>
2018-06-17 06:58:09 -0400

    Frist Commit

---
+++ Desktop/Exericise1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	printf("University of Moratuwa\n");
+	printf("Katubedda,\n");
+	printf("Moratuwa,\n");
+	printf("Sri Lanka\n");
+	printf("----------------------\n");
+	printf("www.mrt.ac.lk\n");
+	return 0;
+}
