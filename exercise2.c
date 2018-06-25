ca16cc14d4a577474fd0cb59f94303a4fd919000
DCKahagalla <30688349+DCKahagalla@users.noreply.github.com>
2018-06-25 07:16:01 -0400

    Add files via upload

---
+++ exercise2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+	printf("%d\n",65*2);
+	printf("%d\n",65/2);
+	printf("%f\n",65.0/2);
+	printf("%c\n",65);
+	printf("%x\n",255);
+
+}
---
+++ exercise3mod.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+	int a=3;
+	float b=10.0;
+	float c;
+	c=b/a;
+	printf("A is %d\n",a);
+	printf("B is %.2f\n",b);
+	printf("Answer is %.2f\n",c);
+
+}
